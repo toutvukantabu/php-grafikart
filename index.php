171eeb5b57f3e05a1b8c992b3c1cb86add3f6643
Gwendal Bescont <64533628+toutvukantabu@users.noreply.github.com>
2020-11-13 17:18:33 +0100

    13/11

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
 <?php
 
-require 'personnage.php';
+// require 'personnage.php';
 
 
-$merlin = new Personnage("merlin");
-
-$merlin->regenerer();
-
-
-$harry=new Personnage("Harry");
+// $merlin = new Personnage("merlin");
+// $merlin->setNom('Melin ');
+// $merlin->regenerer();
 
 
+// $harry=new Personnage("Harry");
 
-var_dump($merlin);
-var_dump($harry);
+// $merlin ->attaque($harry);
+
+// if ($harry->mort()){
+// echo 'Harry est mort :(';
+
+// }else{
+//     echo'harry a survécu avec '. $harry->vie . ' points de vie';
+// }
+// var_dump($merlin->atk);
+// var_dump($harry);
+
+
+require 'form.php';
+
+$form = new Form(array(
+    'username' =>'Roger'
+));
+
+echo $form->input('username');
+echo $form->input('password');
+echo $form->submit();
+
+
+$form = new Form(array(
+    'username' =>'Roger'
+));
+
+echo $form->input('username');
+echo $form->input('password');
+echo $form->input('submit');
+
+
--- personnage.php
+++ personnage.php
@@ -1,33 +1,82 @@
-<?php
+ <?php
 
-class Personnage{
+  // class Personnage
+  // {
 
-//  public $vie = 80;
-//  public $atk=20;
-//  public $nom ;
+  //   public $vie = 80;
+  //   public $atk = 20;
+  //   private $nom;
 
- public function regenerer($vie =null){
+  //   public function regenerer($vie = null)
+  //   {
 
-if (is_null($vie)){
-$this->vie=100;
+  //     if (is_null($vie)) {
+  //       $this->vie = 100;
+  //     } else {
+  //       $this->vie += $vie;
+  //     }
+  //   }
 
-}
-else{
-    $this->vie += $vie;
-}
- }
-    
-public function __construct($nom){
+  //   public function getNom()
+  //   {
 
-    $this->nom = $nom;
-}
+  //     return $this->nom;
+  //   }
 
- public function mort(){
+  //   public function getVIe()
+  //   {
 
-   return  $this->vie <= 0;
+  //     return $this->vie = $vie;
+  //   }
 
- }   
+  //   public function getAtk(){ 
+  //     return $this->atk;
+  //    }
+
+  //   public function setNom()
+  //   {
+
+  //     return $this->nom;
+  //   }
+
+  //   public function setVIe()
+  //   {
+
+  //     return $this->vie = $vie;
+  //   }
+
+  //   public function setAtk()
+  //   {
+
+  //     return $this->atk = $atk;
+  //   }
+
+
+  //   public function __construct($nom)
+  //   {
+
+  //     $this->nom = $nom;
+  //   }
 
 
 
-}
+  //   public function mort()
+  //   {
+
+  //     return  $this->vie <= 0;
+  //   }
+  //   private function empecher_negatif()
+  //   {
+  //     if ($this->vie < 0) {
+
+  //       $this->vie = 0;
+  //     }
+  //   }
+
+  //   public function attaque($cible)
+  //   {
+
+  //     $cible->vie -= $this->atk;
+  //     $cible->empecher_negatif();
+  //   }
+  // }
