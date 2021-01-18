da2a9e6b7f36f22714363484f2b4c99312894668
toutevukantabu <toutevukantabu@users.noreply.github.com>
2021-01-18 23:14:54 +0100

    18/01

---
+++ chapitre 15/index.php
@@ -0,0 +1,13 @@
+<?php 
+$title= "Page d'accueil";
+
+require 'header.php'; ?>
+
+  <div class="starter-template text-center py-5 px-3">
+    <h1>Exercice sur les dates</h1>
+  
+  </div>
+
+
+
+  <?php require 'footer.php'; ?>
